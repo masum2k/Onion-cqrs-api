aa83be676caf4f00393183dcef6319a68afa3ad0
Sidar Orman <sidarormang@gmail.com>
2025-08-08 20:25:28 +0300

    comment

--- Core/Api.Domain/Entitites/Brand.cs
+++ Core/Api.Domain/Entitites/Brand.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//entity
 namespace Api.Domain.Entitites
 {
     public class Brand : EntityBase
