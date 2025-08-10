6f5b3a8d0c4baa1da92771d15818ed793b805d32
Sidar Orman <sidarormang@gmail.com>
2025-08-10 16:14:28 +0300

    mediatr eklendi

---
+++ Presentation/Api.Api/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Api.Application.Features.Products.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await _mediator.Send(new GetAllProductsQueryRequest());
+            return Ok(response);
+        }
+    }
+}
