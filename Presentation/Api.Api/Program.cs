dc0d7be1b9fd8ae63289075fb14320995ea3c6a7
Sidar Orman <sidarormang@gmail.com>
2025-08-13 21:54:56 +0300

    a

--- Presentation/Api.Api/Program.cs
+++ Presentation/Api.Api/Program.cs
@@ -1,5 +1,6 @@
 using Api.Persistence;
 using Api.Application;
+using Api.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,7 @@ builder.Configuration
 
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplicationLayer();
+builder.Services.AddCustomMapper();
 
 var app = builder.Build();
 
