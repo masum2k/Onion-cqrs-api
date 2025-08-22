8eccb929cc3a4a54446be2109e473c063d1560ed
Sidar Orman <sidarormang@gmail.com>
2025-08-22 13:26:38 +0300

    a

---
+++ Core/Api.Application/Features/Products/Command/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Api.Application.Interfaces.AutoMapper;
+using Api.Application.Interfaces.UnitOfWorks;
+using Api.Domain.Entitites;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Application.Features.Products.Command.UpdateProduct
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public UpdateProductCommandHandler(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+        public async Task Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            var products = await unitOfWork.GetReadRepository<Product>()
+                .GetAsync(x=> x.Id == request.Id && !x.IsDeleted);
+
+            var map = mapper.Map<Product,UpdateProductCommandRequest>(request);
+
+            var productCategories = await unitOfWork.GetReadRepository<ProductCategory>()
+                .GetAllAsync(x => x.ProductId == products.Id);
+
+            await unitOfWork.GetWriteRepository<Product>().UpdateAsync(map);
+        }
+
+    }
+}
