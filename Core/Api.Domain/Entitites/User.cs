11859f591d8b2f62b8e771f181354cff3ec3f56f
Sidar Orman <sidarormang@gmail.com>
2025-08-23 13:15:56 +0300

    a

---
+++ Core/Api.Domain/Entitites/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entitites
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string FullName { get; set; }
+        public string? RefreshEventArgs  { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+
+    }
+}
